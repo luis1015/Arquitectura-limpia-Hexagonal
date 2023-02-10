a5a22ffb5c135c75206154795f3ca23c48bee6b4
LuisEDiaz12 <luis.diazes@pragma.com.co>
2023-02-10 00:25:43 -0500

    primer commit hexa

---
+++ arqui_hexagonal/src/main/java/com/example/arqui_hexagonal/ArquiHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.example.arqui_hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiHexagonalApplication.class, args);
+	}
+
+}
